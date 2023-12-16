a715246099e0f642ed3e89642f6752047f3180be
Hyouka0129 <131112800+Hyouka0129@users.noreply.github.com>
2023-12-16 20:10:28 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from model_generator import ResNet,ResidualBlock
+
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+# 加载CIFAR-10数据集
+train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
+
+val_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=4)
+
+# 创建ResNet模型
+model = ResNet(ResidualBlock, [2,2,2])
+model = model.to(device)
+# 定义损失函数和优化器
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+# 训练模型
+num_epochs = 30
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    print(f'Training - Epoch {epoch + 1}/{num_epochs}, Loss: {running_loss / len(train_loader)}')
+    # Validation
+    model.eval()
+    correct = 0
+    total = 0
+    val_loss = 0.0
+
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Validation - Epoch {epoch + 1}/{num_epochs}, Loss: {val_loss / len(val_loader)}, Accuracy: {100 * correct / total}%')
+
+print("Training finished.")
+
+# 保存模型
+torch.save(model.state_dict(), 'resnet_cifar10.pth')
