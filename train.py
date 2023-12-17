1bc70c6618a7499af341021a00b7d5b75c80ebf8
Hyouka0129 <131112800+Hyouka0129@users.noreply.github.com>
2023-12-17 14:35:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -10,29 +10,30 @@ transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.RandomCrop(32, padding=4),
     transforms.ToTensor(),
+    #transforms.ConvertImageDtype(torch.float16),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 # 加载CIFAR-10数据集
 train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
 
 val_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=4)
 
 # 创建ResNet模型
-model = ResNet(ResidualBlock, [2,2,2])
+model = ResNet(ResidualBlock)
 model = model.to(device)
 # 定义损失函数和优化器
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
 # 训练模型
-num_epochs = 30
+num_epochs = 50
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-
+#model = model.to(dtype=torch.float16)
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
