ab7a301f3534791bc1bc6346b6e68d6adb31a554
Hyouka0129 <131112800+Hyouka0129@users.noreply.github.com>
2024-01-05 20:56:41 +0800

    Update train_transformer.py

--- train_transformer.py
+++ train_transformer.py
@@ -4,57 +4,57 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
+from quantized_model import MyViTModel
 from torch.utils.data import DataLoader
-
+from torch.optim.lr_scheduler import CosineAnnealingLR
 transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
-     transforms.RandomCrop(224, padding=4),
+     transforms.Resize((224,224)),
     transforms.ToTensor(),
-    #transforms.ConvertImageDtype(torch.float16),
+    transforms.ConvertImageDtype(torch.bfloat16),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 # 加载Flower102数据集
 train_dataset = datasets.Flowers102(root='./data', split='train', download=True, transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4)
 
 val_dataset = datasets.Flowers102(root='./data', split='val', download=True, transform=transform)
-val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False, num_workers=4)
 test_dataset = datasets.Flowers102(root='./data', split='test', download=True, transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=4)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=4)
 num_classes = 102
 # 创建ResNet模型
-model = models.vit_b_16(weights=models.ViT_B_16_Weights.IMAGENET1K_V1)
+model = MyViTModel(models.vit_b_16(weights=models.ViT_B_16_Weights.IMAGENET1K_V1))
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
-model.heads = nn.Linear(768, num_classes)
+model.vit_model.heads = nn.Linear(768, num_classes)
 model = model.to(device)
+print(torch.max(model.vit_model.encoder.layers.encoder_layer_6.self_attention.in_proj_weight))
+
 print(model)
-for name, param in model.named_parameters():
-    print(name)
-    print(param.shape)
-    param.requires_grad = False
-    if '11.self_attention' in name:
-        print('reactivate')
-        param.requires_grad = True
-    if '10.self_attention' in name:
-        print('reactivate')
-        param.requires_grad = True
-    if 'encoder.ln' in name:
-        print('reactivate')
-        param.requires_grad = True
-    if 'heads' in name:
-        print('reactivate')
-        param.requires_grad = True
-# 定义损失函数和优化器
+with torch.no_grad():
+    for name, param in model.vit_model.named_parameters():
+        print(name)
+        print(param.shape)
+        if 'in_proj_weight' in name:
+            param.data = torch.round(param.data * 256)
+            #print(torch.max(param))
+        if 'in_proj_bias' in name:
+            param.data = torch.round(param.data* 4096)
+        if 'out_proj.weight' in name:
+            param.data = torch.round(param.data * 256)
+        if 'out_proj.bias' in name:
+            param.data = torch.round(param.data*16384)
 print(count_parameters(model))
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
 # 训练模型
-num_epochs = 50
-#model = model.to(dtype=torch.float16)
+num_epochs = 100
+scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=0)
+model = model.to(dtype=torch.bfloat16)
 print("training:")
 for epoch in range(num_epochs):
     model.train()
@@ -66,14 +66,39 @@ for epoch in range(num_epochs):
         loss = criterion(outputs, labels)
         loss.backward()
         for name, param in model.named_parameters():
-            if '9.self_attention' in name:
-                param.grad[:int(len(param.grad) // 2 )] = torch.zeros_like(param.grad[:int(len(param.grad) // 2 ) ])
-            if '10.self_attention' in name:
-                param.grad[:int(len(param.grad) // 2 )] = torch.zeros_like(param.grad[:int(len(param.grad) // 2 )])
-            #if '11.self_attention' in name:
-            #    param.grad[:len(param.grad) // 2] = torch.zeros_like(param.grad[:len(param.grad) // 2])
+            if 'in_proj_weight' in name:
+                param.grad = param.grad * 256 *256
+                #print(torch.mean(param.grad)/torch.mean(param.data))
+            if 'in_proj_bias' in name:
+                param.grad = param.grad * 4096 *4096
+            if 'out_proj.weight' in name:
+                param.grad = param.grad * 256 *256
+                #print(torch.mean(param.grad)/torch.mean(param.data))
+            if 'out_proj.bias' in name:
+                param.grad = param.grad * 16384 *16384
+            if 'mlp' in name:
+                param.grad = torch.zeros_like(param.grad)
         optimizer.step()
         running_loss += loss.item()
+    for name, param in model.named_parameters():
+            if 'in_proj_weight' in name:
+                #print(param)
+                param.data = torch.round(param.data)
+                #print(torch.max(param.data))
+            if 'in_proj_bias' in name:
+                param.data = torch.round(param.data)
+            if 'out_proj.weight' in name:
+                #print(param)
+                param.data = torch.round(param.data)
+                #print(torch.max(param.data))
+            if 'out_proj.bias' in name:
+                param.data = torch.round(param.data)
+    '''for name, param in model.named_parameters():
+        if 'in_proj_weight' in name:
+            #print(torch.max(param))
+            #print(torch.mean(param.grad))
+        if 'in_proj_bias' in name:
+                print(torch.max(param))'''
     # Validation
     model.eval()
     correct = 0
